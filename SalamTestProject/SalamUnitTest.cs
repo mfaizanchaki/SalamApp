fc6800da8d8c1c0b5824a28d01712a4af0950060
mfaizanchaki <faizandada170@gmail.com>
2017-09-25 01:54:12 +0500

    Added Successful Test for SalamToConsole

---
+++ SalamTestProject/SalamUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalamTestProject
+{
+    [TestClass]
+    public class SalamUnitTest
+    {
+        [TestMethod]
+        public void SalamToConsoleTestMethod()
+        {
+            // Arrange
+
+            string expectedResult = "Assalamu alaikum";
+
+            // Act
+
+            string result = ConsoleApp1.Program.SalamToConsole();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
